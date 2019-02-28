148667a12dfa151d9da27b545d12340cdc82bc8b
Yann VAN HECKE <yann.van-hecke@epitech.eu>
2019-02-28 18:22:17 +0100

    adding finshed project

--- predicition.py
+++ predicition.py
@@ -22,15 +22,25 @@ def compute_std_deviation(period = 0, input_list = []):
             return "nan"
     return "nan"
 
+def compute_std_deviation_n(period = 0, input_list = []):
+    if (len(input_list) >= period):
+        try:
+            avg = sum(input_list[-period:]) / period
+            std_dev = sqrt(sum(map(lambda x:(x - avg)**2, input_list[-period:])) / period)
+            return '{:.2f}'.format(std_dev)
+        except (ValueError, FloatingPointError, ZeroDivisionError):
+            return None
+    return None
+
 def compute_mobile_avg(period = 0, input_list = []):
     if (len(input_list) >= period):
         try:
             avg = sum(input_list[-period:]) / period
             std_dev = sum(input_list[-period:]) / period
-            return '{:.2f}'.format(std_dev)
+            return std_dev
         except (ValueError, FloatingPointError, ZeroDivisionError):
-            return "nan"
-    return "nan"
+            return None
+    return None
 
 def compute_increase_avg(period = 0, input_list = []):
     if (len(input_list) > period):
@@ -49,18 +59,23 @@ def compute_switch_status(period, input_list):
             prevalue = round((input_list[-2] / input_list[-(2 + period)] - 1) * 100)
             if (abs(prevalue + value) != abs(prevalue) + abs(value)):
                 tendency_switch += 1
-                return "\ta switch occurs"
+                return "    a switch occurs"
         except (ValueError, FloatingPointError):
             return ""
     return ""
 
 
-def display_final():
+def display_final(pos_list, in_list, period):
     global tendency_switch
     print("Global tendency switched {} times".format(tendency_switch))
+    pos_list = list(map(lambda x : abs(x - .5), pos_list))
+    oth_list = sorted(pos_list)
+    fin_list = oth_list[-5:]
+    fin_list = list(map(lambda x : in_list[pos_list.index(x) + period - 1], fin_list))
+    print("5 weirdest values are {}".format(fin_list[::-1]))
 
 def display_results(period, input_value, input_list):
-    print("g={}\tr={}%\ts={}{}".format(
+    print("g={}    r={}%    s={}{}".format(
     compute_increase_avg(period, input_list),
     compute_relative_evo(period, input_list),
     compute_std_deviation(period, input_list),
@@ -71,15 +86,22 @@ def groundhog(period):
     input_list = []
     std_dev_list = []
     mobile_avg_list = []
+    pos_list = []
     trend_switch_nb = 0
     while (input_value != "STOP"):
         try:
             input_list.append(float(input_value))
             display_results(period, input_value, input_list)
-            std_dev_list.append(compute_std_deviation(period, input_list))
+            std_dev_list.append(compute_std_deviation_n(period, input_list))
             mobile_avg_list.append(compute_mobile_avg(period, input_list))
-            print("\%b", (input_list[-1] - input_list[-1] - float(mobile_avg_list[-1]) - 2 * float(std_dev_list[-1])) / (float(mobile_avg_list[-1]) + 2 * float(std_dev_list[-1]) - input_list[-1] - float(mobile_avg_list[-1]) - 2 * float(std_dev_list[-1])))
+            std_dev_list = list(filter(None, std_dev_list))
+            try:
+                bande_basse = mobile_avg_list[-1] - 2 * float(std_dev_list[-1])
+                bande_haute = mobile_avg_list[-1] + 2 * float(std_dev_list[-1])
+                pos_list.append((input_list[-1] - bande_basse) / (bande_haute - bande_basse))
+            except IndexError:
+                pass
         except ValueError:
             continue
         input_value = input()
-    display_final()
+    display_final(pos_list, input_list, period)
